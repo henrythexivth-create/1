c88150074cde10f8dfa6ae37b28970f808cf799d
henrythexivth-create <henrythexivth@gmail.com>
2026-07-22 17:45:07 -0500

    Add files via upload

--- name.py
+++ name.py
@@ -1,1 +1,7 @@
-print ("hello world")
+#Comment
+print ('I am in 6th grade going to 7th')
+print ('I have 3 pets')
+print ('I like playing games')
+#performing and printing a simple calculation
+print ('the sum of 9 and 10 is', 9+10)
+print (f"the sum of 9 and 10 is {9+10}")
